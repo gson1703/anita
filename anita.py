b6f1ab136d81b669852dd2f3dd62435dc5019c61
gson <gson>
2006-08-01 15:06:02 +0000

    3.0.1

--- anita.py
+++ anita.py
@@ -1,25 +1,35 @@
-#!/usr/bin/python
+#!/usr/pkg/bin/python
 
 import pexpect
 import sys
 import os
+import time
 
-dist="/usr/build/136/release/i386/installation"
+os.system("qemu-img create hd 1G")
 
-os.system("qemu-img create hd 512M");
+def send_slowly(child, str):
+    for char in str:
+        child.send(char)
+        child.expect(char)
 
-# ktrace -f qemu.kt 
-child = pexpect.spawn("qemu -m 32 -hda hd -fda %s/floppy/boot-com1.fs -cdrom %s/cdrom/netbsd-i386.iso -boot a -serial stdio -nographic" % (dist, dist))
-#child.setecho(True)
+#dist="/usr/build/136/release/i386/installation"
+#child = pexpect.spawn("qemu -m 32 -hda hd -fda %s/floppy/boot-com1.fs -cdrom %s/cdrom/netbsd-i386.iso -boot a -serial stdio -nographic" % (dist, dist))
+
+child = pexpect.spawn("qemu -m 32 -hda hd pkg.hd -fda boot-com1.fs -cdrom i386cd-3.0.1.iso -boot a -serial stdio -nographic")
+
 child.log_file = sys.stdout
+child.timeout = 3600
 
 child.expect("insert disk 2, and press return...")
+# Escape into qemu command mode
 child.send("\001c")
-child.expect("(qemu)")
-child.send("change fda %s/floppy/boot2.fs\n" % dist)
-child.expect("(qemu)")
+child.expect('\(qemu\)')
+#send_slowly(child, "change fda %s/floppy/boot2.fs" % dist)
+send_slowly(child, "change fda boot2.fs")
+child.send("\n")
+child.expect('\(qemu\)')
+# Exit qemu command mode
 child.send("\001c\n")
-# set timeout -1
 child.expect("a: Installation messages in English")
 child.send("\n")
 child.expect("Keyboard type")
@@ -40,9 +50,11 @@ child.expect("b: Use the entire disk")
 child.send("b\n")
 child.expect("Do you want to install the NetBSD bootcode")
 child.expect("a: Yes")
+
+child.interact()
+
 child.send("\n")
-#child.#expec( "a: Set sizes of NetBSD partitions")
-# XXX if you select "b: Use existing partition sizes" here, things go wrong
+# XXX If you select "b: Use existing partition sizes" here, things go wrong
 child.send("\n")
 child.expect("Accept partition sizes")
 child.send("\016\016\016\016\016\016\016\016\n")
@@ -57,9 +69,6 @@ child.expect("b: Use serial port com0")
 # XXX sysinst is inconsistent here; you must select "x" to exit
 # the dialog, whereas other similar dialogs let you just press enter.
 child.send("bx\n")
-# Here, sysinst runs /bin/cp /usr/mdec/boot /targetroot/boot
-#child.#expec( "Hit enter to continue")
-#child.#sen( "\n")
 child.expect("a: Progress bar")
 child.send("\n")
 child.expect("a: CD-ROM")
@@ -86,6 +95,4 @@ child.send("x\n")
 child.expect("#")
 child.send("halt\n")
 child.expect("halted by root")
-
-# interact
 
