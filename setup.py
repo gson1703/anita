f2cbdccef808e4d8b77e2a285847e07a182f98f4
gson <gson>
2012-03-12 19:12:04 +0000

    1.25 release marker

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='anita',
-      version='1.24',
+      version='1.25',
       description='Automated NetBSD Installation and Test Application',
       author='Andreas Gustafsson',
       author_email='gson@gson.org',
