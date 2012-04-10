8646c2b0bd949fbf3efc6f82d3b37e24192df101
gson <gson>
2012-04-10 13:15:48 +0000

    1.27

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='anita',
-      version='1.26',
+      version='1.27',
       description='Automated NetBSD Installation and Test Application',
       author='Andreas Gustafsson',
       author_email='gson@gson.org',
