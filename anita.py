249217bf7549e6255a09b8172a4dc956afdc70af
gson <gson>
2013-10-03 17:56:53 +0000

    version 1.31

--- anita.py
+++ anita.py
@@ -13,7 +13,7 @@ import time
 import urllib
 import urlparse
 
-__version__='1.30'
+__version__='1.31'
 
 # Your preferred NetBSD FTP mirror site.
 # This is used only by the obsolete code for getting releases
