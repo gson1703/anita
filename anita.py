384c976b6625494528570f731c74f638800288de
gson <gson>
2017-01-22 22:04:09 +0000

    Increase default child timeout from 300 to 600 seconds.

--- anita.py
+++ anita.py
@@ -745,7 +745,7 @@ class Anita:
 	child.logfile_read = multifile([self.unstructured_log_f, Logger('recv', self.structured_log_f)])
 	# Log writes to child
 	child.logfile_send = multifile([self.unstructured_log_f, Logger('send', self.structured_log_f)])
-        child.timeout = 300
+        child.timeout = 600
         child.setecho(False)
         # Xen installs sometimes fail if we don't increase this
 	# from the default of 0.1 seconds.  And powering down noemu
