9cd5a14178cde1195e5b07ba78f4f06160f7fd73
Andreas Gustafsson <gson@araneus.fi>
2017-07-12 23:04:32 +0300

    version 1.42

--- anita.py
+++ anita.py
@@ -13,7 +13,7 @@ import time
 import urllib
 import urlparse
 
-__version__='1.41'
+__version__='1.42'
 
 # Your preferred NetBSD FTP mirror site.
 # This is used only by the obsolete code for getting releases
