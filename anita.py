f086be95e1b1e6343bb3dd1cb6dfa8475440dbd8
Andreas Gustafsson <gson@araneus.fi>
2019-07-15 17:38:03 +0300

    bump __version__ to 1.49b

--- anita.py
+++ anita.py
@@ -28,7 +28,7 @@ try:
 except ImportError:
     from pipes import quote as sh_quote
 
-__version__='1.49a'
+__version__='1.49b'
 
 # Your preferred NetBSD FTP mirror site.
 # This is used only by the obsolete code for getting releases
