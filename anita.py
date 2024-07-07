50ad4908cb0c626ec8c6533a5fede9c86c6401bc
Andreas Gustafsson <gson@araneus.fi>
2024-07-07 19:13:32 +0300

    version 2.14

--- anita.py
+++ anita.py
@@ -36,7 +36,7 @@ if sys.version_info[0] >= 3:
     import functools
     print = functools.partial(print, flush = True)
 
-__version__='2.13'
+__version__='2.14'
 
 # Your preferred NetBSD FTP mirror site, and the archive site used for
 # obtaining older releases.
